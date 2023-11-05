77da66ef8212514243a5d5be42ca67d04e82d805
dayas-kr-coder <dayaskr2005@gmail.com>
2023-11-06 01:34:20 +0530

    ADDED A LOT OF THINGS  Created section-2  Created Pricing section  Created footer  CSS improved

---
+++ app.js
@@ -0,0 +1,19 @@
+const login = document.querySelector('.login');
+
+login.addEventListener('click', () => {
+  login.innerHTML = `<div class="profile">
+  <img src="./images/dayas-kr.jpg" alt="" />
+</div>`;
+});
+
+let a = `
+
+
+
+
+
+
+
+
+
+`
