a198b753d5eb4bf2babbe4beb57034991f658a3d
dayas-kr-coder <dayaskr2005@gmail.com>
2023-11-07 20:29:40 +0530

    CHANGES NAV FOOTER
     Added hand-burger menu
     Made footer responsive

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ login.addEventListener('click', () => {
   <img src="./images/dayas-kr.jpg" alt="" />
 </div>`;
 });
+
+const burger = document.querySelector('.burger');
+const navbarNav = document.querySelector('.navbar-nav');
+
+burger.addEventListener('click', () => {
+  navbarNav.classList.toggle('nav-active');
+});
